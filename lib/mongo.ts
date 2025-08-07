f8197816414c96b4b277a2b58e5c7f577a369d34
david <david@mac.home>
2025-08-07 17:31:01 +0200

    reset

--- lib/mongo.ts
+++ lib/mongo.ts
@@ -1,8 +1,11 @@
 import { Database, MongoClient } from "mongo/mod.ts";
 import "jsr:@std/dotenv/load";
 
-const uri = Deno.env.get("MONGO");
-const DB = "t";
+const uri = Deno.env.get("MONGO_URI");
+const DB = Deno.env.get("MONGO_DB_NAME");
+
+const uri2 =
+  "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.6";
 
 let db: Database;
 
@@ -10,12 +13,10 @@ async function createMongoDbConnection() {
   try {
     const client = new MongoClient();
     await client.connect(uri as string);
-    console.log(`Mongo db URI/.... DB : ${uri}`);
     console.log(`Mongo db connection established/.... DB : ${DB}`);
     return client.database(DB);
   } catch (error) {
     console.log("error");
-    console.log(`Mongo db URI/.... DB : ${uri}`);
     throw new Error(error as any);
   }
 }
--- main.ts
+++ main.ts
@@ -5,28 +5,12 @@
 /// <reference lib="deno.ns" />
 
 import "$std/dotenv/load.ts";
-import { Database, MongoClient } from "mongo/mod.ts";
-import "jsr:@std/dotenv/load";
+
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
 
-const uri = Deno.env.get("MONGO");
-const DB = "t";
-
-export async function createMongoDbConnection() {
-  try {
-    const client = new MongoClient();
-    await client.connect(uri as string);
-
-    console.log(`Mongo db connection established/.... DB yee : ${DB}`);
-    return client.database(DB);
-  } catch (error) {
-    console.log("error");
-    console.log(`Mongo db URI/.... DB : ${uri}`);
-    throw new Error(error as any);
-  }
-}
+import { createMongoDbConnection } from "./lib/mongo.ts";
 
 await createMongoDbConnection();
 
